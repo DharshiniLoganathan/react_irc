09815c84f415a339897cad8c53d6d720de2dfa73
DharshiniLoganathan <118422159+DharshiniLoganathan@users.noreply.github.com>
2023-12-14 12:38:40 +0530

    Add files via upload

---
+++ login/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './components/Login'; // Assuming LoginForm.js is in the same directory
+
+const App = () => {
+  return (
+    <div>
+
+      <Login />
+    </div>
+  );
+};
+
+export default App;
